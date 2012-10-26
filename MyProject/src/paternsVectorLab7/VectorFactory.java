7938de828cca0900e3adb5122357703b44840960
akrygin <krygina.ua@gmail.com>
2012-10-26 18:31:41 +0200

    New Test commit

---
+++ MyProject/src/paternsVectorLab7/VectorFactory.java
@@ -0,0 +1,11 @@
+package paternsVectorLab7;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Admin
+ * Date: 26.10.12
+ * Time: 18:25
+ * To change this template use File | Settings | File Templates.
+ */
+public interface VectorFactory {
+}
