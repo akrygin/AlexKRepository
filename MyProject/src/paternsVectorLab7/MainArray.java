9bcd70c763b82867dc971efc507e635162b770b0
akrygin <vasya12345098>
2012-10-27 22:45:17 +0300

    for bit

--- MyProject/src/paternsVectorLab7/MainArray.java
+++ MyProject/src/paternsVectorLab7/MainArray.java
@@ -1,9 +1,10 @@
 package paternsVectorLab7;
 
 public class MainArray {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Vector c = Vectors.createInstance(6);
         c.setVectorFromArray(Vectors.randomVector(6));
         System.out.println(c);
-	}
+        System.out.println("For bitbucket test");
+    }
 }
